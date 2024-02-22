fae75336910822793917e6b24ee02fe5c85734fe
TheUser1935 <52703921+TheUser1935@users.noreply.github.com>
2024-02-22 20:26:37 +1100

    Update Contract.sol

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.18;
 
-contract Contract { }
+contract DemoContract { 
+    uint256 a = 1;
+    uint256 b = 9;
+    uint256 result;
+
+    function calculate() public {
+        result = a + b;
+    }
+
+}
